d95f922cb61a906643cfeceddbd179d5d9a68e9a
mJamshid1990 <mahmudov.jamshid@mail.ru>
2022-06-05 14:26:28 +0500

    add js codes

---
+++ js/main.js
@@ -0,0 +1,399 @@
+let ism = "Jamshid";
+let familiya = "Maxmudov";
+
+let data = {
+  people: [
+    {
+      id: 1,
+      first_name: "Anvar",
+      last_name: "Narzullayev",
+      avatar: [
+        "https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.ytimg.com%2Fvi%2FN_DpQtz5ZR4%2Fmaxresdefault.jpg&imgrefurl=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DN_DpQtz5ZR4&tbnid=Eg-lbcNOQE81YM&vet=12ahUKEwiu1om-w5X4AhWExosKHZA3DlQQMygNegUIARCEAQ..i&docid=i9C1y56pvYUcNM&w=1280&h=720&q=anvar%20narzullayev&ved=2ahUKEwiu1om-w5X4AhWExosKHZA3DlQQMygNegUIARCEAQ",
+        "https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.ytimg.com%2Fvi%2FN_DpQtz5ZR4%2Fmaxresdefault.jpg&imgrefurl=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DN_DpQtz5ZR4&tbnid=Eg-lbcNOQE81YM&vet=12ahUKEwiu1om-w5X4AhWExosKHZA3DlQQMygNegUIARCEAQ..i&docid=i9C1y56pvYUcNM&w=1280&h=720&q=anvar%20narzullayev&ved=2ahUKEwiu1om-w5X4AhWExosKHZA3DlQQMygNegUIARCEAQ#imgrc=Eg-lbcNOQE81YM&imgdii=1W8p1cV2ZFdcmM",
+        "https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.ytimg.com%2Fvi%2FN_DpQtz5ZR4%2Fmaxresdefault.jpg&imgrefurl=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DN_DpQtz5ZR4&tbnid=Eg-lbcNOQE81YM&vet=12ahUKEwiu1om-w5X4AhWExosKHZA3DlQQMygNegUIARCEAQ..i&docid=i9C1y56pvYUcNM&w=1280&h=720&q=anvar%20narzullayev&ved=2ahUKEwiu1om-w5X4AhWExosKHZA3DlQQMygNegUIARCEAQ#imgrc=1W8p1cV2ZFdcmM&imgdii=t2POGWm0AyYS6M",
+        "https://www.google.com/imgres?imgurl=https%3A%2F%2Flookaside.fbsbx.com%2Flookaside%2Fcrawler%2Fmedia%2F%3Fmedia_id%3D872023573473499&imgrefurl=https%3A%2F%2Fne-np.facebook.com%2Fnajottalim%2Fposts%2Fuzoq-kutilgan-master-klass-anvar-narzulloh-bilankoplab-taklif-va-talablarni-inob%2F872024233473433%2F&tbnid=iaDV-VvfHqMYJM&vet=12ahUKEwiu1om-w5X4AhWExosKHZA3DlQQMygiegUIARCyAQ..i&docid=wtjamX5_CbVuiM&w=1440&h=1440&q=anvar%20narzullayev&ved=2ahUKEwiu1om-w5X4AhWExosKHZA3DlQQMygiegUIARCyAQ#imgrc=iaDV-VvfHqMYJM&imgdii=t6usRSNGGR8gtM",
+      ],
+      bio: "python, sun'iy intellect, mohirdev.uz",
+      phone_number: "+99800 000 00 00",
+      user_name: "@sariqdev",
+      groups_common: 4,
+      shared_links: 15,
+      shared_files: 1,
+      shared_photos: 20,
+      shared_voice: 15,
+      activity: "online",
+      messages: [
+        {
+          id: 1,
+          is_from_me: false,
+          text: "Hello",
+          time: new Date(2021, 7, 16, 11, 10, 05).getTime(),
+        },
+        {
+          id: 2,
+          is_from_me: true,
+          text: "Hi",
+          time: new Date(2021, 7, 16, 11, 11, 10).getTime(),
+        },
+        {
+          id: 3,
+          is_from_me: false,
+          text: "How are you ?",
+          time: new Date(2021, 7, 16, 11, 11, 15).getTime(),
+        },
+        {
+          id: 4,
+          is_from_me: true,
+          text: "🙂 and you ?",
+          time: new Date(2021, 7, 16, 11, 12, 50).getTime(),
+        },
+      ],
+    },
+    {
+      id: 2,
+      first_name: "Saidbek",
+      last_name: "Arislonov",
+      avatar: [
+        "https://www.google.com/imgres?imgurl=https%3A%2F%2Fstorage.kun.uz%2Fsource%2F6%2FYg2eaAPcNUad6YPLHKR4LHPwWmrxQTr8.jpg&imgrefurl=https%3A%2F%2Fkun.uz%2Fru%2Fnews%2F2021%2F01%2F31%2Fot-pomoshchnika-ofitsianta-do-senior-programmista-beseda-s-uzbekskim-parnem-rabotayushchim-v-mastercard&tbnid=3DLOcG1I6lOUWM&vet=12ahUKEwjU-Pa63ZX4AhUMzCoKHZ4cBDIQMygCegQIARBL..i&docid=zLCADuGhTArgFM&w=834&h=515&q=saidbek%20arislonov&ved=2ahUKEwjU-Pa63ZX4AhUMzCoKHZ4cBDIQMygCegQIARBL",
+        "https://www.google.com/imgres?imgurl=https%3A%2F%2Fstorage.kun.uz%2Fsource%2F6%2FYg2eaAPcNUad6YPLHKR4LHPwWmrxQTr8.jpg&imgrefurl=https%3A%2F%2Fkun.uz%2Fru%2Fnews%2F2021%2F01%2F31%2Fot-pomoshchnika-ofitsianta-do-senior-programmista-beseda-s-uzbekskim-parnem-rabotayushchim-v-mastercard&tbnid=3DLOcG1I6lOUWM&vet=12ahUKEwjU-Pa63ZX4AhUMzCoKHZ4cBDIQMygCegQIARBL..i&docid=zLCADuGhTArgFM&w=834&h=515&q=saidbek%20arislonov&ved=2ahUKEwjU-Pa63ZX4AhUMzCoKHZ4cBDIQMygCegQIARBL#imgrc=3DLOcG1I6lOUWM&imgdii=yBHfN7rIJPGIHM",
+      ],
+      bio: "java script, selecon valley",
+      phone_number: "+99800 123 45 67",
+      user_name: "@s_arislonov",
+      groups_common: 3,
+      shared_links: 11,
+      shared_files: 1,
+      shared_photos: 15,
+      shared_voice: 10,
+      activity: "Last seen today at 10:01 AM",
+      messages: [
+        {
+          id: 1,
+          is_from_me: false,
+          text: "Salomatmisiz",
+          time: new Date(2021, 7, 16, 11, 10, 05).getTime(),
+        },
+        {
+          id: 2,
+          is_from_me: true,
+          text: "Rahmat o'ziz qaleysiz",
+          time: new Date(2021, 7, 16, 11, 11, 10).getTime(),
+        },
+      ],
+    },
+    groups: [
+      {
+        id: 1,
+        name: "MERN va PERN F-13",
+        avatar: [],
+        bio: "Vazifalar uchun: @mernperntasksbot",
+        user_name: "private_group_mern",
+        members: 7,
+        shared_links: 15,
+        shared_files: 16,
+        shared_photos: 16,
+        shared_voice: 0,
+        shared_videos: 11,
+        activity: " members, 2 online",
+        messages: [
+          {
+            id: 1,
+            is_from_me: false,
+            text: "lorem ipsum dolor sit amet...",
+            time: new Date(2021, 8, 1, 11, 11, 11).getTime(),
+          },
+          {
+            id: 2,
+            is_from_me: true,
+            text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias error fuga possimus cum, ab animi quaerat autem omnis fugiat mollitia delectus perferendis quo, placeat, quis totam maxime quae voluptatem esse?",
+            time: new Date(2021, 8, 1, 11, 11, 50).getTime(),
+          },
+        ],
+      },
+    ],
+    channels: [
+      {
+        id: 1,
+        name: "Mirzayev's Mind",
+        avatar: [
+          "https://static10.tgstat.ru/channels/_0/b0/b06d62818baaf237d626e5360ea562c7.jpg",
+        ],
+        bio: `
+              ▪️ Entrepreneur
+              ▪️ Investor
+              ▪️ Developer
+
+              Omad yordir dovyuraklarga! • Судьба благоволит храбрым! • Fates Favors the Brave!
+              Murojaat uchun: @Odilbek_Mirzayev_aloqabot`,
+        user_name: "@OdilbekMirzayev",
+        members: 1657,
+        shared_links: 94,
+        shared_files: 1,
+        shared_photos: 72,
+        shared_voice: 3,
+        shared_videos: 9,
+        activity: " subscribers",
+        messages: [
+          {
+            id: 1,
+            is_from_me: false,
+            text: "lorem ipsum dolor sit amet...",
+            time: new Date(2021, 8, 1, 11, 11, 11).getTime(),
+          },
+          {
+            id: 2,
+            is_from_me: false,
+            text: "lorem ipsum dolor sit amet...",
+            time: new Date(2021, 8, 2, 11, 11, 11).getTime(),
+          },
+        ],
+      },
+    ],
+    bots: [
+      {
+        id: 1,
+        name: "VKM Bot",
+        avatar: [
+          "https://botostore.com/netcat_files/22/26/preview_26957_1542499501.jpg",
+          "https://picsum.photos/500/384",
+          "https://picsum.photos/490/384",
+          "https://picsum.photos/480/384",
+          "https://picsum.photos/470/384",
+          "https://picsum.photos/400/384",
+        ],
+        bio: `
+              Listen to and download any music!
+              Contact & Ads: @vkmowner
+              Our media: @vekaem`,
+        user_name: "@vkm_bot",
+        shared_links: 22,
+        shared_files: 0,
+        shared_photos: 0,
+        shared_voice: 417,
+        activity: "bot",
+        messages: [
+          {
+            id: 1,
+            is_from_me: false,
+            text: "lorem ipsum dolor sit amet...",
+            time: new Date(2021, 8, 1, 11, 11, 11).getTime(),
+          },
+          {
+            id: 2,
+            is_from_me: true,
+            text: "Mustafa Ceceli",
+            time: new Date(2021, 8, 1, 11, 11, 11).getTime(),
+          },
+        ],
+      },
+    ],
+    me: [
+      {
+        first_name: ism,
+        last_name: familiya,
+        avatar: ["https://uroljon.github.io/Khidirboev/img/back.png"],
+        bio: `CEO at uroljon.ml | lotin-kirill.ga`,
+        phone_number: "+99 890 152 75 11",
+        user_name: "@Uroljon_Khidirboev",
+        shared_links: 10,
+        shared_files: 20,
+        shared_photos: 30,
+        shared_voice: 0,
+        activity: "Online",
+        messages: [
+          {
+            id: 1,
+            is_from_me: true,
+            text: "lorem ipsum dolor sit amet...",
+            time: new Date(2021, 8, 1, 11, 11, 11).getTime(),
+          },
+          {
+            id: 2,
+            is_from_me: true,
+            text: "This is Saved Messages section. You can use it for your own private cloud. Enjoy Telegram :)",
+            time: new Date(2021, 8, 1, 11, 11, 11).getTime(),
+          },
+        ],
+      },
+      {
+        first_name: "Please select any chat to start messaging",
+        last_name: "",
+        avatar: [
+          "https://res.cloudinary.com/jerrick/image/upload/v1613548658/602ccc72b2f2e2001df1a885.png",
+        ],
+        bio: ``,
+        phone_number: "not available",
+        user_name: "@deleted_account",
+        shared_links: 0,
+        shared_files: 0,
+        shared_photos: 0,
+        shared_voice: 0,
+        shared_videos: 0,
+        activity: "Last seen a long time ago",
+        messages: [
+          {
+            id: 1,
+            is_from_me: false,
+            is_service_notifications: true,
+            text: "This account has just been deleted",
+            time: new Date().getTime(),
+          },
+        ],
+      },
+    ],
+  ],
+  groups: [
+    {
+      id: 1,
+      name: "Backend-20",
+      avatar: [],
+      bio: "JS ga asoslangan beckend kursi",
+      user_name: "@nodeJs",
+      members: 7,
+      shared_links: 15,
+      shared_files: 16,
+      shared_photos: 16,
+      shared_voice: 0,
+      shared_videos: 11,
+      activity: " members, 2 online",
+      messages: [
+        {
+          id: 1,
+          is_from_me: false,
+          text: "lorem ipsum dolor sit amet...",
+          time: new Date(2021, 8, 1, 11, 11, 11).getTime(),
+        },
+        {
+          id: 2,
+          is_from_me: true,
+          text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias error fuga possimus cum, ab animi quaerat autem omnis fugiat mollitia delectus perferendis quo, placeat, quis totam maxime quae voluptatem esse?",
+          time: new Date(2021, 8, 1, 11, 11, 50).getTime(),
+        },
+      ],
+    },
+  ],
+  channels: [
+    {
+      id: 1,
+      name: "Art-Kokand",
+      avatar: [
+        "https://www.google.com/imgres?imgurl=https%3A%2F%2Fstatic10.tgstat.ru%2Fchannels%2F_0%2F0b%2F0b012b9f451776131e5287874717f82a.jpg&imgrefurl=https%3A%2F%2Ftgstat.com%2Fru%2Fchannel%2F%40artschoolkokand&tbnid=CIuSbZhMeY16JM&vet=12ahUKEwii77rC35X4AhXJvCoKHU0pA2oQMygAegUIARDCAQ..i&docid=KobpOHdUy69-RM&w=640&h=640&itg=1&q=art-kokand&ved=2ahUKEwii77rC35X4AhXJvCoKHU0pA2oQMygAegUIARDCAQ",
+      ],
+      bio: `
+      Mazkur kanal Qõqon ixtisoslashtirilgan san'at maktabi uchun maxsus yaratilgan bõlib, bunda maktabda õtkazilgan tadbirlar, yangiliklar hamda topshiriqlar muntazam yoritib boriladi.
+      `,
+      user_name: "@artschoolkokand",
+      members: 506,
+      shared_links: 94,
+      shared_files: 1,
+      shared_photos: 72,
+      shared_voice: 3,
+      shared_videos: 9,
+      activity: " subscribers",
+      messages: [
+        {
+          id: 1,
+          is_from_me: false,
+          text: "lorem ipsum dolor sit amet...",
+          time: new Date(2021, 8, 1, 11, 11, 11).getTime(),
+        },
+        {
+          id: 2,
+          is_from_me: false,
+          text: "lorem ipsum dolor sit amet...",
+          time: new Date(2021, 8, 2, 11, 11, 11).getTime(),
+        },
+      ],
+    },
+  ],
+  bots: [
+    {
+      id: 1,
+      name: "VKM Bot",
+      avatar: [
+        "https://botostore.com/netcat_files/22/26/preview_26957_1542499501.jpg",
+        "https://picsum.photos/500/384",
+      ],
+      bio: `
+            Listen to and download any music!
+            Contact & Ads: @vkmowner
+            Our media: @vekaem`,
+      user_name: "@vkm_bot",
+      shared_links: 22,
+      shared_files: 0,
+      shared_photos: 0,
+      shared_voice: 417,
+      activity: "bot",
+      messages: [
+        {
+          id: 1,
+          is_from_me: false,
+          text: "lorem ipsum dolor sit amet...",
+          time: new Date(2021, 8, 1, 11, 11, 11).getTime(),
+        },
+        {
+          id: 2,
+          is_from_me: true,
+          text: "Mustafa Ceceli",
+          time: new Date(2021, 8, 1, 11, 11, 11).getTime(),
+        },
+      ],
+    },
+  ],
+  me: [
+    {
+      first_name: ism,
+      last_name: familiya,
+      avatar: ["https://uroljon.github.io/Khidirboev/img/back.png"],
+      bio: `Yaratganga shukur`,
+      phone_number: "+99 890 588 99 10",
+      user_name: "@jamshidartschool",
+      shared_links: 20,
+      shared_files: 30,
+      shared_photos: 40,
+      shared_voice: 0,
+      activity: "Online",
+      messages: [
+        {
+          id: 1,
+          is_from_me: true,
+          text: "lorem ipsum dolor sit amet...",
+          time: new Date(2021, 8, 1, 11, 11, 11).getTime(),
+        },
+        {
+          id: 2,
+          is_from_me: true,
+          text: "Lorem ipsum",
+          time: new Date(2021, 8, 1, 11, 11, 11).getTime(),
+        },
+      ],
+    },
+    {
+      first_name: "Please select any chat to start messaging",
+      last_name: "",
+      avatar: [
+        "https://res.cloudinary.com/jerrick/image/upload/v1613548658/602ccc72b2f2e2001df1a885.png",
+      ],
+      bio: ``,
+      phone_number: "not available",
+      user_name: "@deleted_account",
+      shared_links: 0,
+      shared_files: 0,
+      shared_photos: 0,
+      shared_voice: 0,
+      shared_videos: 0,
+      activity: "Last seen a long time ago",
+      messages: [
+        {
+          id: 1,
+          is_from_me: false,
+          is_service_notifications: true,
+          text: "This account has just been deleted",
+          time: new Date().getTime(),
+        },
+      ],
+    },
+  ],
+};
+
+!localStorage.getItem("data")&&localStorage.setItem("data",JSON.stringify(data))
